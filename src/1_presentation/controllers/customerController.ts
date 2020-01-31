a8b6df6d168f184592ddad758cac4932da4d562d
rrubiszewski <rafal.rubiszewski@gmail.com>
2020-01-31 15:48:48 +0100

    Prepare customer controller

--- src/1_presentation/controllers/customerController.ts
+++ src/1_presentation/controllers/customerController.ts
@@ -1,13 +1,13 @@
 import * as express from "express";
 import { inject } from "inversify";
 import { request,
-  controller, httpDelete, httpGet, httpPost, httpPut, requestParam,
+  controller, httpDelete, httpGet, httpPost, httpPut, requestParam, httpPatch, queryParam,
 } from "inversify-express-utils";
 import TYPES from "./../../4_infrastructure/container/TYPES";
 import { Customer } from "./../../3_domain/models/customer";
 import { ICustomerService } from "./../../2_application/customer/customerServiceInterface";
 
-@controller("/customers")
+@controller("/customer")
 export class CustomerController {
     private customerService: ICustomerService;
 
@@ -16,7 +16,61 @@ export class CustomerController {
   }
 
   @httpGet("/")
-  public getAllProducts(): Customer[] {
+  public getAllCutomers(): Customer[] {
       return this.customerService.getAll();
+  }
+
+  @httpGet("/:id")
+  public getCustomerById(@requestParam("id") id: string): Customer {
+      return null;
+  }
+
+  @httpPost("/")
+  public createCustomer(@request() req: express.Request): Customer {
+      return null;
+  }
+
+  @httpPut("/:id")
+  public updateCustomer(@requestParam("id") id: string, @request() req: express.Request): Customer {
+      return null;
+  }
+
+  @httpDelete("/:id")
+  public deleteCustomer(@requestParam("id") id: string): Customer {
+      return null;
+  }
+
+  @httpPost("/address/customer/:id")
+  public addAddress(@requestParam("id") id: string, @request() req: express.Request): boolean {
+      return false;
+  }
+
+  @httpDelete("/address/customer/:id")
+  public deleteAddress(@requestParam("id") id: string, @request() req: express.Request): boolean {
+      return false;
+  }
+
+  @httpPatch("/:id/email")
+  public updateCustomerEmail(@queryParam("email") start: string): Customer {
+      return null;
+  }
+
+  @httpPost("/:id/cart")
+  public createCart(@requestParam("id") id: string, @request() req: express.Request): Customer {
+      return null;
+  }
+
+  @httpPut("/:id/cart")
+  public addItemToCart(@requestParam("id") id: string, @request() req: express.Request): Customer {
+      return null;
+  }
+  @httpDelete("/:id/cart")
+  public removeItemFromCart(@requestParam("id") id: string, @request() req: express.Request): Customer {
+      return null;
+  }
+
+  @httpDelete("/:id/cart/empty")
+  public emptyCart(@requestParam("id") id: string): Customer {
+      return null;
   }
 }
