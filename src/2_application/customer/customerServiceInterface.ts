56630a4964a7d01d80132a23978cc14246cfcd77
RRubisze <rafal.rubiszewski@gmail.com>
2020-02-11 16:27:57 +0100

    Add basic usage for customers

--- src/2_application/customer/customerServiceInterface.ts
+++ src/2_application/customer/customerServiceInterface.ts
@@ -4,7 +4,7 @@ import { Bill } from "./../../3_domain/models/bill";
 
 export interface ICustomerService {
     getAll(): Customer[];
-    getById(id: string): Customer[];
+    getById(id: string): Customer;
     createCustomer(customer: Customer): Customer;
     deleteCustomer(id: string): boolean;
     updateCustomer(id: string, model: Customer): Customer;
